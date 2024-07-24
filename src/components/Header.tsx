984940b138ddb372a649a8c31136b409aee8ca0f
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-07-24 08:39:13 -0500

    created Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,29 @@
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Header = ({ children }: HeaderProps) => {
   return (
-    <div className='header'>
-      <Link href={'/'}></Link>
+    <div className="header">
+      <Link href={"/"}>
+        <Image
+          src={"/assets/icons/logo.svg"}
+          alt="Logo with name"
+          width={120}
+          height={32}
+          className="hidden md:block"
+        />
+        <Image
+          src={"/assets/icons/logo-icon.svg"}
+          alt="Logo"
+          width={32}
+          height={32}
+          className="hidden md:block"
+        />
+      </Link>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
