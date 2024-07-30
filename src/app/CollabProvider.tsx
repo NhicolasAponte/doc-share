f3629eec8796802c507d44fdcc7fcb70ddf1ffd2
NhicolasAponte <Nhic.Aponte@gmail.com>
2024-07-30 09:29:08 -0500

    - stubbed in user mention feature, need to implement collaborators before testing

--- src/app/CollabProvider.tsx
+++ src/app/CollabProvider.tsx
@@ -1,19 +1,33 @@
 "use client";
-import React, { use } from "react";
+import React from "react";
 import {
   ClientSideSuspense,
   LiveblocksProvider,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/server-actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/server-actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 
 const CollabProvider = ({ children }: { children: React.ReactNode }) => {
+
+  const { user: clerkUser } = useUser();
+
   return (
     <LiveblocksProvider 
       authEndpoint={"/api/liveblocks-auth"}
       resolveUsers={async ({ userIds }) => {
         const users = await getClerkUsers({ userIds });
         return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const clerkUser = await getClerkUsers({ userIds: [] });
+        const docUsers = await getDocumentUsers(
+          roomId,
+          clerkUser?.emailAddresses[0].emailAddress,
+          text,
+        );
+
+        return docUsers;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
--- src/lib/server-actions/user.actions.ts
+++ src/lib/server-actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { clerkClient } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
     try {
@@ -24,5 +25,24 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
     }
     catch (error) {
         console.error(`Error occurred while fetching clerk users: `, error);
+    }
+}
+
+export async function getDocumentUsers(roomId: string, currentUser: string, text: string) {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+
+        const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser);
+
+        if(text.length) {
+            const lowerCaseText = text.toLowerCase();
+            const filteredUsers = users.filter((email) => email.toLowerCase().includes(lowerCaseText));
+            return parseStringify(filteredUsers);
+        }
+
+        return parseStringify(users);
+    }
+    catch (error) {
+        console.error(`Error occurred while fetching document users: `, error);
     }
 }
